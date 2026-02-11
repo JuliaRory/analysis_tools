8c619408e1d6e8cccbe709068fb6e374dd63a40d
MEG RESEARCHER <shevtsova.jg@gmail.com>
2026-02-11 13:19:14 +0300

    updates for 10 electrodes with dry gel test

--- run_spectr_analysis.py
+++ run_spectr_analysis.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from src.utils.parse_h5df import load_h5df
-from src.utils.spectral_analysis import bandpass_filter, compute_psd_welch
+from src.utils.spectral_analysis import bandpass_filter, compute_psd_welch, compute_windowed_fft
 from src.utils.montage_processing import find_ch_idx
 from src.utils.rereferencing import rereference_eeg
 from src.visualization.plot_signal import plot_signal
+from src.visualization.spectrogram import plot_spectrogram
 from src.utils.transformations import unit_to_db
-from src.visualization.check_alpha_rhythm import plot_spectr
+from src.visualization.check_alpha_rhythm import plot_spectr, plot_alpha_spectr
 
 # ch_labels = get_channel_names(CED_FILE)
 # positions = get_topo_positions(CED_FILE)
@@ -16,12 +17,12 @@ from src.visualization.check_alpha_rhythm import plot_spectr
 
 # idx_Fz = find_ch_idx("Fz", CED_FILE)
 
-DATASET = "pilot/01_calib_sessions"
-SESSION = "02ES_ses10.12.2025_calib_v1.1"
-DATA_FOLDER = os.path.join(r".\data\raw\pilot\01_calib_sessions", SESSION, "data")
+DATA_FOLDER = r".\data"
 RECORD = "01-open-closed-eyes.hdf"
 
-EEG_CHANNELS = np.arange(64)
+EEG_CHANNELS = np.arange(10)
+REFERENT_CHANNELS = [11, 12]    # or NONE
+
 Fs = 1000 # Hz
 s_to_idx = lambda x: int(x * Fs)
 ms_to_idx = lambda x: int(x // 1000 * Fs)
@@ -29,6 +30,13 @@ ms_to_idx = lambda x: int(x // 1000 * Fs)
 CED_FILE = r"./resources/mks64_standard.ced"
 labels_ROA = ["FC5", "FC3", "FC1", "C1", "CP1", "CP3", "CP5", "C5", "C3"] # mu rhythm
 labels_ROA = ["PO7", "PO3", "POz", "PO4", "PO8", "O1", "Oz", "O2", "Fz", "FCz", "AF3", "AF4"] # occipital lobe and frontal electrodes
+
+# =================================
+# ==========DRY GEL TEST===========
+# =================================
+CED_FILE = r"./resources/mks10.ced"
+labels_ROA = ["PO3", "POz", "PO4", "O1", "Oz", "O2", "Fz", "Cz", "P5", "P6"] # occipital lobe and frontal electrodes
+
 idxs_ROA = [find_ch_idx(ch, CED_FILE) for ch in labels_ROA]
 
 plt.ion() 
@@ -50,8 +58,13 @@ filt_eeg = bandpass_filter(raw_eeg, fs=Fs, low=0.5, high=40)
 
 # re-referencing Fz
 print("---re referencing---")
-idx_Fz = find_ch_idx("Fz", CED_FILE)
-reref_eeg = rereference_eeg(filt_eeg, idx_Fz)
+if REFERENT_CHANNELS is None:
+    idx_Fz = find_ch_idx("Fz", CED_FILE)
+    reref_eeg = rereference_eeg(filt_eeg, idx_Fz)
+else:
+    # idx_Fz = find_ch_idx("Fz", CED_FILE)
+    idx_ref = REFERENT_CHANNELS
+    reref_eeg = rereference_eeg(filt_eeg, idx_ref)
 
 signal = filt_eeg if not do_referencing else reref_eeg
 
@@ -61,13 +74,34 @@ plot_signal(start_s, end_s, signal, s_to_idx, plot=True)  # plot all channels
 
 # === spectral analysis ===
 print("---psd calculation---")
-freq, psd = compute_psd_welch(signal, fs=Fs, fmin=0.5, fmax=40, freq_res=.5)
-psd_db = unit_to_db(psd)
+# freq, psd = compute_psd_welch(signal, fs=Fs, fmin=0.5, fmax=40, freq_res=.5)
+# psd_db = unit_to_db(psd)
 
-print(np.min(psd_db), np.max(psd_db))
-plot_spectr(freq, psd[idxs_ROA], labels_ROA, plot_mean=True, freq_min = 0, freq_max=30, y_min=np.min(psd), y_max=np.max(psd), to_db=False)
-plot_spectr(freq, psd_db[idxs_ROA], labels_ROA, plot_mean=True, freq_min = 0, freq_max=30, y_min=np.min(psd_db), y_max=np.max(psd_db), to_db=True)
+# print(np.min(psd_db), np.max(psd_db))
+# plot_spectr(freq, psd[idxs_ROA], labels_ROA, plot_mean=True, freq_min = 0, freq_max=30, y_min=np.min(psd), y_max=np.max(psd), to_db=False)
+# plot_spectr(freq, psd_db[idxs_ROA], labels_ROA, plot_mean=True, freq_min = 0, freq_max=30, y_min=np.min(psd_db), y_max=np.max(psd_db), to_db=True)
 
+idx_half = len(raw_eeg) // 2
+eeg_opened = signal[:idx_half, :]       # opened eyes
+eeg_closed = signal[idx_half:, :]       # closed eyes
+
+freq, psd_opened = compute_psd_welch(eeg_opened, fs=Fs, fmin=0.5, fmax=40, freq_res=.5)
+freq, psd_closed = compute_psd_welch(eeg_closed, fs=Fs, fmin=0.5, fmax=40, freq_res=.5)
+
+fig = plot_alpha_spectr(freq, psd_opened[idxs_ROA], psd_closed[idxs_ROA], labels_ROA,  plot_mean=True,
+                 freq_min = 0, freq_max=20, to_db=False)
+
+
+f, t, S = compute_windowed_fft(reref_eeg, fs=Fs)
+
+fig, ax = plot_spectrogram(f, t, S, 
+                           average=True, channels=idxs_ROA,
+                           fmin=1, fmax=40, 
+                           title=f"EEG Spectrogram\n(average of channels: {', '.join(labels_ROA)})",
+                           symmetric=True)
+ax.axvline(idx_half / Fs, color='white')
+ax.text(20, 37, "Opened eyes", color='white', fontsize=20)
+ax.text(80, 37, "Closed eyes", color='white', fontsize=20)
 
 plt.ioff()     
 plt.show()     
