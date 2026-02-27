1fe9afe3a117bd35f8b3538dbc098e2e488d5da4
JuliaRory <shevtsova.jg@gmail.com>
2026-02-27 19:00:44 +0300

    notebook drafts

--- src/utils/CSP.py
+++ src/utils/CSP.py
@@ -1,6 +1,55 @@
 import numpy as np
 from scipy.linalg import eigh
 
+from sklearn.covariance import MinCovDet
+import scipy.linalg as la
+
+# ===================
+# == Анатолий-like ==
+# ===================
+
+def calculate_robust_cov(epochs):
+    """
+    epochs [n_trials, n_samples, n_channels]
+    Return:
+        cov [n_channels, n_channels]
+    """
+    data = np.concatenate(epochs, axis=0)   # [n_samples, n_channels]
+    MCD = MinCovDet(support_fraction=0.5, store_precision=False)
+    cov = MCD.fit(data)
+    return cov
+
+def calculate_CSP(c1, c2):
+    """
+    c1, c2: covariance matrix
+    Return:
+        W_fixed:        
+        projForward:    
+        evals:          
+
+    """
+    R1 = c1 / np.trace(c1)
+    R2 = c2 / np.trace(c2)
+    L, W = la.eig(R1, R1+R2)
+    order = np.argsort(L)
+    L = L[order]
+    W = W[:, order]
+    fProj = np.dot(W.T, R1).T
+    d, p = fProj.shape
+    maxind = np.argmax(np.abs(fProj), axis=0)
+    # maxinds = np.array([np.where(np.abs(W[:, i]) == np.max(np.abs(W[:, i])))[0][0] for i in range(W.shape[1])])
+    max_magnitudes = np.array([fProj[maxind[i], i] for i in range(W.shape[1])])
+    rowsign = np.sign(max_magnitudes)
+    W_fixed = W * rowsign
+    projForward = la.pinv(W_fixed).T
+    evals = L
+    return W_fixed, projForward, evals
+
+# ======================
+# == basic principles ==
+# ======================
+
+
 def cov_epoch(X):
     """
     X: (channels, time)
@@ -11,7 +60,7 @@ def cov_epoch(X):
 def regularize(C, alpha=0.05):
     return (1 - alpha) * C + alpha * np.eye(C.shape[0])
 
-def calculate_CSP(epochs_motor, epochs_rest):
+def calculate_CSP_in_trials(epochs_motor, epochs_rest):
     covs_motor = np.array([cov_epoch(ep.T) for ep in epochs_motor])  # ep: (time, ch)
     C_motor  = covs_motor.mean(axis=0)
     C_motor  = regularize(C_motor,  alpha=0.05)
--- src/visualization/plot_csp_components.py
+++ src/visualization/plot_csp_components.py
@@ -1,6 +1,6 @@
 from matplotlib import colormaps as cm
 from matplotlib.colors import ListedColormap
-
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import matplotlib.pyplot as plt 
 import numpy as np 
@@ -10,39 +10,57 @@ from mne.viz import plot_topomap
 viridisBig = cm.get_cmap('jet')
 newcmp = ListedColormap(viridisBig(np.linspace(0, 1, 15)))
 
+# топографические карты
+def plot_topoplot(X, positions, vmin=None, vmax=None, ch_labels=None, axes=None):
+        im, cn = plot_topomap(X, positions,  image_interp='cubic', ch_type='eeg', names =ch_labels,
+                size=5, show=False, contours=4, sphere=0.5, 
+                cmap=newcmp, extrapolate='head', axes=axes, vlim=[vmin, vmax])
+        return im
 
-def plot_CSP_components(eigvals, A, positions, ch_labels, row_idx, gs, fig):
-        # первый график: линия eigenvalues
-        ax0 = plt.subplot(gs[row_idx, 0])
-        ax0.plot(eigvals, "k")
-        ax0.scatter(range(len(eigvals)), eigvals, marker="o", s=20)
-        ax0.set_ylim(0, 1)
-        ax0.set_title("Eigenvalues")
-        
-        # топоплоты
+def plot_components(projForward, xy, ch_labels, gs=None, row_ind=None, idxs=None):
         ims = []
-        idxs = [0, 1, 2, 3, -4, -3, -2, -1]
-        vmin = np.min(A[idxs])
-        vmax = np.max(A[idxs])
+        if idxs is None:
+                idxs = [0, 1, 2, 3, -4, -3, -2, -1]
+        vmin, vmax = np.min(projForward[:, idxs]), np.max(projForward[:, idxs])
         vmin, vmax = -max(abs(vmin), abs(vmax)), max(abs(vmin), abs(vmax))
         
-         # топографические карты
-        def plot_topoplot(X, positions, vmin=None, vmax=None, ch_labels=None, axes=None):
-                im, cn = plot_topomap(X, positions,  image_interp='cubic', ch_type='eeg', names =ch_labels,
-                        size=5, show=False, contours=4, sphere=0.5, 
-                        cmap=newcmp, extrapolate='head', axes=axes, vlim=[vmin, vmax])
-                return im
-        
         for i, idx in enumerate(idxs):
-                ax_map = plt.subplot(gs[row_idx, i+1])
-                im = plot_topoplot(A[idx], positions, axes=ax_map, 
+                ax_map = plt.subplot(gs[row_ind, i+1])
+                im = plot_topoplot(projForward[:, idx], xy, axes=ax_map, 
                         vmin=vmin, vmax=vmax)
                 comp_number = idx if idx >= 0 else len(ch_labels)+idx
                 ax_map.set_title(f"CSP #{comp_number+1}")
                 ims.append(im)
+        
+        return ims, vmin, vmax
 
+def plot_eigenvalues(eigvals, ax):
+        ax.plot(eigvals, "k")
+        ax.scatter(range(len(eigvals)), eigvals, marker="o", s=20)
+        ax.set_ylim(0, 1)
+        ax.set_title("Eigenvalues")
+
+        ediff = np.diff(eigvals)
+        ok_steps = np.where(ediff > np.median(ediff) * 5)[0]
+        ok_evalLow_inds = np.arange(np.max(ok_steps[ok_steps < 10]))
+        ok_evalHigh_inds = np.arange(len(eigvals)-1, np.min(ok_steps[ok_steps > 30]), -1)
+        
+        ax.scatter(ok_evalLow_inds, eigvals[ok_evalLow_inds],  label='ERD OK')
+        ax.scatter(ok_evalHigh_inds, eigvals[ok_evalHigh_inds], label='ERS OK')
+
+
+
+def plot_CSP_components(eigvals, A, positions, ch_labels, row_idx, gs, fig):
+        # первый график: линия eigenvalues
+
+        ax0 = plt.subplot(gs[row_idx, 0])
+        plot_eigenvalues(eigvals, ax0)
+        
+        # топоплоты
+        ims, vmin, vmax = plot_components(A, positions, ch_labels, gs, row_idx)
+        
         # общий colorbar справа от последнего topomap
-        from mpl_toolkits.axes_grid1 import make_axes_locatable
+        ax_map = plt.subplot(gs[row_idx, len(ims)])
         divider = make_axes_locatable(ax_map)
         cax = divider.append_axes("right", size="5%", pad=0.05)
         cbar = fig.colorbar(ims[-1], cax=cax)
